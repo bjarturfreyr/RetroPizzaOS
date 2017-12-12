3d4cac20ee77d6b7d2408ba6cc330dd2d9fa836c
Bjartur Freyr <bjarturbf3@gmail.com>
2017-12-12 13:06:58 +0000

    remove egill folder

--- egill/RetroPizzaOS/Helpers/GlobalHelpers.cpp
+++
@@ -1,9 +0,0 @@
-#include "GlobalHelpers.h"
-
-void GlobalHelpers::clearScreen(){
-    system("cls");
-}
-
-void GlobalHelpers::setColor(int color){
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
